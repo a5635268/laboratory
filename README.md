9062d54b82139df0cc21841b2f38b8fe492d2b3a
zhouxiaogang <xiaogang.zhou@qq.com>
2019-09-11 16:23:40 +0800

    java测试

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # laboratory
-用于学习python，java，go，es/ts的一个代码管理集合
+
+用于学习python，java，go，es/ts的一个代码试验集合
+
+
+
+
---
+++ java/src/grammar/Hello.java
@@ -0,0 +1,9 @@
+package grammar;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("Hello, world2!");
+        System.out.println("Hello, world3!");
+    }
+}
